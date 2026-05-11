2c7bd6f7fd20bae74c15ec07e6bf492ce7a9a29e
DarkBeider20 <stevexd2016@gmail.com>
2026-05-11 18:57:28 -0500

    feat(i18n): config translation support

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,5 +1,4 @@
 ﻿import { createI18n } from 'vue-i18n';
-import { merge } from 'lodash';
 
 // Importaciones de idiomas globales
 import enGlobal from '../locales/en.json';
@@ -9,10 +8,23 @@ import esGlobal from '../locales/es.json';
 import enSecurity from '@/operational-security-management/locales/en.json';
 import esSecurity from '@/operational-security-management/locales/es.json';
 
+/**
+ * Función simple para fusionar dos objetos (reemplaza a lodash.merge para evitar dependencias extra)
+ */
+function deepMerge(target, source) {
+  for (const key in source) {
+    if (source[key] instanceof Object && key in target) {
+      Object.assign(source[key], deepMerge(target[key], source[key]));
+    }
+  }
+  Object.assign(target || {}, source);
+  return target;
+}
+
 // Fusión profunda de los mensajes
 const messages = {
-  en: merge({}, enGlobal, enSecurity),
-  es: merge({}, esGlobal, esSecurity)
+  en: deepMerge({ ...enGlobal }, enSecurity),
+  es: deepMerge({ ...esGlobal }, esSecurity)
 };
 
 const i18n = createI18n({
