68bc03008b266f70684087e2948ca3229b11cfc9
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-05-24 10:20:35 -0500

    feat(entity): add new entity classes for audit logs, distributor movements, inventory movements, and related models

---
+++ src/inventory-management/domain/model/distributor-movement.entity.js
@@ -0,0 +1,30 @@
+﻿/**
+ * @summary entity class representing a domain model within the application.
+ * Stores and manages the main attributes of a business object.
+ * @class DistributorMovement
+ * @author Kevin Lopez
+ */
+
+export class DistributorMovement {
+    constructor({
+                    id = null,
+                    timestamp = null,
+                    tipo = '',
+                    tipoBalonLabel = '',
+                    cantidad = 0,
+                    cantidadSign = null,
+                    proveedorTipoSalida = '',
+                    responsableNombre = '',
+                    responsableIsOwner = false,
+                }) {
+        this.id = id
+        this.timestamp = timestamp
+        this.tipo = tipo
+        this.tipoBalonLabel = tipoBalonLabel
+        this.cantidad = cantidad
+        this.cantidadSign = cantidadSign
+        this.proveedorTipoSalida = proveedorTipoSalida
+        this.responsableNombre = responsableNombre
+        this.responsableIsOwner = responsableIsOwner
+    }
+}
