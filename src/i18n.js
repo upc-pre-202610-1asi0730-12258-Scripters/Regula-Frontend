0717c639adf3f2133b0e925c6d1f20515005e38b
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-05-24 11:57:03 -0500

    refactor(i18n): update import paths for localization files

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
-﻿import en from '../locales/en.json'
-import es from '../locales/es.json'
+﻿import en from './locales/en.json'
+import es from './locales/es.json'
 import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
